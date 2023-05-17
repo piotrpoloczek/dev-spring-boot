716fef20ab431cbc5326c10a370614bf001a3517
Piotr Poloczek <piotr.jan.poloczek@gmail.com>
2023-05-17 20:44:58 +0200

    add bean-lifecycle-methods

---
+++ 02-spring-boot-spring-core/02-07-bean-scopes/src/main/java/com/luv2code/springcoredemo/rest/DemoController.java
@@ -0,0 +1,37 @@
+package com.luv2code.springcoredemo.rest;
+
+import com.luv2code.springcoredemo.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Primary;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    // define a private field for the dependency
+    private Coach myCoach;
+    private Coach myAnotherCoach;
+
+
+    @Autowired
+    public DemoController(
+            @Qualifier("cricketCoach") Coach theCoach,
+            @Qualifier("cricketCoach") Coach anotherCoach
+    ) {
+        myCoach = theCoach;
+        myAnotherCoach = anotherCoach;
+    }
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout() {
+        return myCoach.getDailyWorkout();
+    }
+
+    @GetMapping("/check")
+    public String check() {
+        return "Comparing beans: myCoach == anotherCoach, " + (myCoach == myAnotherCoach);
+    }
+
+}
