8f3c4e13be0d284e03ca0188a89965ee655c29fc
Piotr Poloczek <piotr.jan.poloczek@gmail.com>
2023-06-04 14:47:38 +0200

    implement DAO

---
+++ 04-spring-boot-rest-crud/04-01-spring-boot-rest-crud/src/main/java/com/luv2code/demo/rest/StudentRestController.java
@@ -0,0 +1,51 @@
+package com.luv2code.demo.rest;
+
+import com.luv2code.demo.entity.Student;
+import jakarta.annotation.PostConstruct;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class StudentRestController {
+
+    private List<Student> theStudents;
+
+    // define @PostConstructor to load the student data ... only once!
+
+    @PostConstruct
+    public void loadData() {
+        theStudents = new ArrayList<>();
+        theStudents.add(new Student("Poornima", "Patel"));
+        theStudents.add(new Student("Poornim21a", "Pa21tel"));
+        theStudents.add(new Student("P44oornima", "Pate44l"));
+    }
+
+    // define endpoints for "/students" - return a list of students
+    @GetMapping("/students")
+    public List<Student> getStudents() {
+        return theStudents;
+    }
+
+    // define endpoint for "/students/{studentId}" - return student at index
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId) {
+
+        // just index into the list .. keep it simple for now
+
+        // check the studentId aganist list size
+
+        if ( (studentId >= theStudents.size()) || (studentId < 0)) {
+            throw new StudentNotFoundException("Student id not found - " + studentId);
+        }
+        return theStudents.get(studentId);
+    }
+
+    // Add an exception handler using @ExceptionHandler
+
+
+}
